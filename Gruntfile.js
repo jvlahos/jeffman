00d2f1de0064aec5e872772d480f6898f007f99e
Jeff Vlahos <jeff@jeffvlahos.com>
2015-06-28 23:31:47 -0400

    Grunt sass, watch, and postcss/autoprefixer working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,81 +1,73 @@
-/*global module:false*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  require('time-grunt')(grunt);
 
-  // Project configuration.
   grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
+      pkg: grunt.file.readJSON('package.json'),
+      sass: {
+        dev: {
+          options: {
+            style: 'expanded',
+            lineNumbers: true,
+            sourcemap: 'none'
+          },
+          files: [{
+            expand: true,
+            cwd: 'scss/',
+            src: ['*.scss'],
+            dest: 'css/',
+            ext: '.css'
+          }]
+        },
+        dist: {
+          options: {
+            style: 'compressed',
+            lineNumbers: false,
+            sourcemap: 'none'
+          },
+          files: [{
+            expand: true,
+            cwd: 'scss/',
+            src: ['*.scss'],
+            dest: 'css/',
+            ext: '.css'
+          }]
+        }
       },
-      dist: {
-        src: ['lib/<%= pkg.name %>.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
-      }
-    }
+      watch: {
+        scripts: {
+          files: ['scss/*.scss'],
+          tasks: ['sass'],
+          options: {
+            spawn: false,
+          },
+        },
+      }, //eo watch
+      postcss: {
+        options: {
+          processors: [
+            require('autoprefixer-core')({
+              browsers: 'last 2 versions, ie >= 8'
+            })
+          ]
+        },
+        dist: {
+          files: [{
+            expand: true,
+            cwd: 'css/',
+            src: ['screen.css'],
+            dest: 'css/',
+            ext: '.css'
+            }]
+        }
+      } //eo postcss
   });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('dev', ['sass:dev', 'postcss']);
+  grunt.registerTask('dist', ['sass:dist', 'postcss']);
+  grunt.registerTask('default', ['sass:dev', 'postcss']);
 
 };
